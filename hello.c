51ef07365ad033866602ecc0568050e6a159267b
Tanner Zimmerman <tzimmerman15@huskers.unl.edu>
2022-08-25 20:45:21 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+* Author: Tanner Zimmmerman
+* Date:8/25/2022
+*
+*A simple Hello World program in C
+*
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Tanner Zimmerman");
+
+    return 0;
+}
